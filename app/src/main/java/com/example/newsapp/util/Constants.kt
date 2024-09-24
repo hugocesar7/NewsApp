333d921dd9d45fc1fc28dd58860fced1745b5479
Hugo Ribeiro <hugocesar.ribeiro@gmail.com>
2024-09-24 19:57:33 -0300

    Retrofit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "abc217cce246461fb8607f278e0be3bf"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
