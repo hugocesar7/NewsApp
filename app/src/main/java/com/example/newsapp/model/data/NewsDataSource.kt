de3f2a85454fe7d404df68c55c6b1a8b83c1f418
Hugo Ribeiro <hugocesar.ribeiro@gmail.com>
2024-09-26 17:03:09 -0300

    Update NewsDataSource.kt

--- app/src/main/java/com/example/newsapp/model/data/NewsDataSource.kt
+++ app/src/main/java/com/example/newsapp/model/data/NewsDataSource.kt
@@ -1,4 +1,25 @@
 package com.example.newsapp.model.data
 
+import com.example.newsapp.network.RetrofitInstance
+import com.example.newsapp.presenter.news.NewsHome
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 class NewsDataSource {
+
+    fun getBreakingNews(callback: NewsHome.Presenter) {
+        GlobalScope.launch(Dispatchers.Main) {
+            val response = RetrofitInstance.api.getBreakingNews("br")
+            if (response.isSuccessful) {
+                response.body()?.let { newsResponse ->
+                    callback.onSuccess(newsResponse)
+                }
+                callback.onCompleted()
+            } else {
+                callback.onError(response.message())
+                callback.onCompleted()
+            }
+        }
+    }
 }
